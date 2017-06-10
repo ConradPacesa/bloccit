6182cb18c8b472e5d15e77097ed0e09f16fca4d2
Conrad Pacesa <conradpacesa@gmail.com>
2017-06-10 00:58:49 +0000

    Added user scope to posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,19 @@
 require 'random_data'
 
+#create users
+5.times do 
+    User.create!(
+    name: RandomData.random_name, 
+    email: RandomData.random_email, 
+    password: RandomData.random_sentence
+    )
+end
+users = User.all
 #create topics
 15.times do 
     Topic.create!(
-        name: RandomData.random_sentence,
-        description: RandomData.random_paragraph
+    name: RandomData.random_sentence,
+    description: RandomData.random_paragraph
     )
 end
 topics = Topic.all
@@ -12,9 +21,10 @@ topics = Topic.all
 #create posts 
 50.times do 
     Post.create!(
-        topic: topics.sample,
-        title: RandomData.random_sentence, 
-        body: RandomData.random_paragraph
+    user: users.sample,
+    topic: topics.sample,
+    title: RandomData.random_sentence, 
+    body: RandomData.random_paragraph
     )
 end
 posts = Post.all
@@ -22,12 +32,19 @@ posts = Post.all
 #create comments 
 100.times do 
     Comment.create!(
-        post: posts.sample, 
-        body: RandomData.random_paragraph
+    post: posts.sample, 
+    body: RandomData.random_paragraph
     )
 end
 
+user = User.first
+user.update_attributes!(
+    email: 'conradpacesa@gmail.com',
+    password: 'password'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
